64075f7b86467b42b2393416ef1f99c9fc8e0e09
Badhon_khan <bkhan256048@gmail.com>
2026-07-05 14:22:33 +0600

    initial commit

---
+++ UniMart/martapp/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Register(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    username = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    phone = models.IntegerField(max_length=11)
+    password = models.CharField(max_length=150)
+    is_active = models.BooleanField(blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    create_by = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Register'
+
+    def __str__(self):
+        return self.username
+    
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(unique=True)
+    description = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'category'
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=300)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    price = models.IntegerField()
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='media/image')
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'Product'
+
+    def __str__(self):
+        return self.name
+
---
+++ UniMart/martapp/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render,redirect
+from . import models
+from django.contrib.auth import login as auth_login
+# Create your views here.
+def user_login(request):
+    print(request.method)
+    if request.method =='POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        if not models.User.objects.filter(email=email).exists():
+            return render(request,'login.html',{'error':"email does not exists!"})
+        
+        user = models.User.objects.get(email=email)
+
+        if not user.check_password(password):
+            return render(request,'login.html',{'error':"password must be same!"})
+        auth_login(request,user)
+        return redirect('home')
+    return render(request,'login.html')
+
+def register(request):
+    if request.method=='POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        # phone = request.POST.get('phone')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        is_active = bool(request.POST.get('is_active'))
+
+        if models.User.objects.filter(email=email).exists():
+            return render(request,'register.html',{'error':"email allready exists!"})
+        
+        if not password1 == password2:
+            return render(request,'register.html',{'error':"password does not same!"})
+
+        model = models.User.objects.create_user(
+            first_name = first_name,
+            last_name = last_name,
+            username= username,
+            email=email,
+            # phone= phone,
+            password=password1,
+            is_active=is_active
+        )
+        return redirect('home')
+
+    return render(request,'register.html')
+
+def home(request):
+    
+    return render(request,'home.html')
